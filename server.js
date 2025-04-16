0f87e8540c792021814f2146583a3d1f19723972
Novaxux <manuelova@outlook.es>
2025-04-15 21:14:01 -0600

    changes documentation an changed html link stylesheet

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const app = express();
 const productRoutes = require("./routes/products.routes");
 const homeRoutes = require("./routes/home.routes");
 const logRequests = require("./middleware/logRequests");
+// settings
 app.set("port", 3002);
 // matters uppercase and lowercase in routing
 app.set("case sensitive routing", true);
-app.use(express.json());
-app.use("/public", express.static(path.join(__dirname, "public")));
 // middleware
 app.use(logRequests);
+app.use(express.json());
+// routes
 app.use(productRoutes);
 app.use(homeRoutes);
+// static files
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.listen(app.get("port"), () =>
   console.log(`Servidor en http://localhost:${app.get("port")}`)
